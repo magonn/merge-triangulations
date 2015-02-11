413b675b5ed2fe9e4c70add8692f4deea26f17cc
magonn <gotman.maria@gmail.com>
2015-02-12 00:50:38 +0300

    MST for 2 triangle
    Okay, I did all simple work, than I must to write algorithm

--- main.py
+++ main.py
@@ -2,8 +2,3 @@ from triangulationClass import *
 
 app = triangulationClass(600, 400)
 app.root.mainloop()
-
-#print("edges", app.edges)
-#print("openEdges", app.openEdges)
-#print("points", len(app.points))
-#print("faces", app.faces)
